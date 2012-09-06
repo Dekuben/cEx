1fb9cedb769b7e8eabf99dcefcf9a481d6c8273b
dekuben <dekuben@gmail.com>
2012-09-06 01:30:57 +0100

    first attempt at combining both conditions

--- test.sh
+++ test.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #script to compile database program and test
 set -e
-if [ -f ex17.c ];
+#-f is an argument of test function, here represented as square brackets. it returns true of succesful
+if [ -f ex17.c ]&&[! -f ex17];
 then
 	echo ex17.c found
+	echo ex17 not found
 else
 	echo ex17.c not found
+	echo or ex17 found
 fi
